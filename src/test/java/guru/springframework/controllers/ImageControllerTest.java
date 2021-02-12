94f47a0d99299c6c1b0531c0b5711899771eef6f
akashjagdale <adjagdale25@gmail.com>
2021-02-13 01:47:40 +0530

    Display image from DBbb

--- src/test/java/guru/springframework/controllers/ImageControllerTest.java
+++ src/test/java/guru/springframework/controllers/ImageControllerTest.java
@@ -9,12 +9,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.mockito.Mockito.anyLong;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.any;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -62,6 +64,32 @@ public class ImageControllerTest {
 				.andExpect(header().string("Location", "/recipe/1/show"));
 
 		verify(imageService, times(1)).saveImageFile(anyLong(), any());
+	}
+
+	@Test
+	public void testRenderImageFormDB() throws Exception {
+
+		RecipeCommand recipeCommand = new RecipeCommand();
+		recipeCommand.setId(1L);
+
+		String s = "some string";
+		Byte[] byteArray = new Byte[s.getBytes().length];
+
+		int i = 0;
+		for (byte b : s.getBytes()) {
+			byteArray[i++] = b; // Auto unboxing
+		}
+
+		recipeCommand.setImage(byteArray);
+
+		when(recipeService.findCommandById(anyLong())).thenReturn(recipeCommand);
+
+		MockHttpServletResponse response = mockMvc.perform(get("/recipe/1/recipeimage")).andExpect(status().isOk())
+				.andReturn().getResponse();
+
+		byte[] responseBytes = response.getContentAsByteArray();
+
+		assertEquals(s.getBytes().length, responseBytes.length);
 	}
 
 }
